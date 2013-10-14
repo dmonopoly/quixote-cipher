d1823404fe180fec426626c1523eff690b0b5319
David Zhang <dzhang21@gmail.com>
2013-10-14 15:08:19 -0700

    tentative tag grammar finder implemented

--- TagGrammarFinder.cc
+++ TagGrammarFinder.cc
@@ -5,10 +5,18 @@
 #include <fstream>
 #include <map>
 #include <vector>
+#include <set>
+
+#include "em-training-example/Notation.h"
 
 using namespace std;
 
-const string KEY_DELIM = "|";
+// TODO: could be moved to a better spot
+const string GIVEN_DELIM = "|";
+const string AND_DELIM = ",";
+const string SEQ_DELIM = "";
+const string SIGMA = "SUM_i "; // key helper for Notation
+const string ARB_SOUND_PLACEHOLDER = "s_i";
 
 int main() {
   string filename = "GivenCounts/corpus.spanish.sound_bigrams";
@@ -16,7 +24,10 @@ int main() {
   if (fin.fail())
     cerr << "Could not open file " << filename << endl;
   else {
-    map<string, int> bigram_counts;
+    map<string, double> data;
+    set<string> sounds;
+    map<string, int> bigram_counts; // Key: Notation string repr.
+    // Read bigram counts from file.
     int count;
     string sound1, sound2;
     while (true) {
@@ -27,7 +38,28 @@ int main() {
       // Remove quotes.
       sound1 = sound1.substr(1, sound1.size() - 2);
       sound2 = sound2.substr(1, sound2.size() - 2);
-      bigram_counts[sound2 + KEY_DELIM + sound1] += count;
+      sounds.insert(sound1);
+      sounds.insert(sound2);
+      Notation n("C", {sound1}, SEQ_DELIM, {sound2});
+      Notation nc_total(SIGMA + "C", {sound1}, SEQ_DELIM,
+          {ARB_SOUND_PLACEHOLDER});
+      bigram_counts[n.repr()] += count;
+    }
+    fin.close();
+
+    // Determine tag grammar probabilities.
+    for (auto s1 = sounds.begin(); s1 != sounds.end(); ++s1) {
+      for (auto s2 = sounds.begin(); s2 != sounds.end(); ++s2) {
+        Notation n("P", {*s2}, GIVEN_DELIM, {*s1});
+        Notation nc("C", {*s1}, SEQ_DELIM, {*s2});
+        Notation nc_total(SIGMA + "C", {*s1}, SEQ_DELIM,
+            {ARB_SOUND_PLACEHOLDER});
+        data[n.repr()] = (double) bigram_counts.at(nc.repr()) /
+          bigram_counts.at(nc_total.repr());
+      }
+    }
+    for (auto it = data.begin(); it != data.end(); ++it) {
+      cout << "data " << it->first << " = " << it->second << endl;
     }
   }
 
