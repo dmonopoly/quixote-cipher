aa1cf0e4862e747ce42c932d8b4a23a262267e6a
David Zhang <dzhang21@gmail.com>
2013-10-15 21:39:37 -0700

    reads bigram probabilitie (looks okay)

--- TagGrammarFinder.cc
+++ TagGrammarFinder.cc
@@ -11,15 +11,21 @@
 
 using namespace std;
 
+#define EXTRA_PRINTING false
+
 // TODO: could be moved to a better spot
 const string GIVEN_DELIM = "|";
 const string AND_DELIM = ",";
-const string SEQ_DELIM = "";
+const string SEQ_DELIM = " ";
 const string SIGMA = "SUM_i "; // key helper for Notation
 const string ARB_SOUND_PLACEHOLDER = "s_i";
 
-int main() {
-  string filename = "GivenCounts/corpus.spanish.sound_bigrams";
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cerr << "No filename given." << endl;
+    return 0;
+  } 
+  string filename = argv[1];
   ifstream fin(filename.c_str());
   if (fin.fail())
     cerr << "Could not open file " << filename << endl;
@@ -35,15 +41,18 @@ int main() {
       if (fin.eof())
         break;
       fin >> sound1 >> sound2;
+      if (EXTRA_PRINTING)
+        cout << "Read " << count << " " << sound1 << " " << sound2 << endl;
       // Remove quotes.
       sound1 = sound1.substr(1, sound1.size() - 2);
       sound2 = sound2.substr(1, sound2.size() - 2);
       sounds.insert(sound1);
       sounds.insert(sound2);
-      Notation n("C", {sound1}, SEQ_DELIM, {sound2});
+      Notation nc("C", {sound1}, SEQ_DELIM, {sound2});
       Notation nc_total(SIGMA + "C", {sound1}, SEQ_DELIM,
           {ARB_SOUND_PLACEHOLDER});
-      bigram_counts[n.repr()] += count;
+      bigram_counts[nc.repr()] = count; // should only encounter once 
+      bigram_counts[nc_total.repr()] += count;
     }
     fin.close();
 
@@ -54,8 +63,24 @@ int main() {
         Notation nc("C", {*s1}, SEQ_DELIM, {*s2});
         Notation nc_total(SIGMA + "C", {*s1}, SEQ_DELIM,
             {ARB_SOUND_PLACEHOLDER});
-        data[n.repr()] = (double) bigram_counts.at(nc.repr()) /
-          bigram_counts.at(nc_total.repr());
+
+        // If no key found, then just set to 0.
+        if (bigram_counts.find(nc.repr()) == bigram_counts.end() || bigram_counts.find(nc_total.repr()) ==
+            bigram_counts.end()) {
+          if (EXTRA_PRINTING) {
+            if (bigram_counts.find(nc.repr()) == bigram_counts.end())
+              cout << "Not found: " << nc.repr() << endl;
+            if (bigram_counts.find(nc_total.repr()) == bigram_counts.end())
+              cout << "Not found: " << nc_total.repr() << endl;
+          }
+          data[n.repr()] = 0;
+        } else {
+          if (EXTRA_PRINTING)
+            cout << bigram_counts.at(nc.repr()) << "/" << bigram_counts.at(nc_total.repr()) << endl;
+          double val = (double) bigram_counts.at(nc.repr()) /
+                        bigram_counts.at(nc_total.repr());
+          data[n.repr()] = val;
+        }
       }
     }
     for (auto it = data.begin(); it != data.end(); ++it) {
